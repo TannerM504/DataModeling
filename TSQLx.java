81e1bde2a7c6c2c669268553cb91fc16e10579d4
TannerM504 <N00935492@ospreys.unf.edu>
2017-03-29 14:32:00 -0400

    Update TSQLx.java

--- TSQLx.java
+++ TSQLx.java
@@ -3,12 +3,19 @@ public class TSQLx{
   public static void main(String [] args){
     
   }
-  public static void createDatabase(String dataBaseName){
   
+  public static void createDatabase(String dataBaseName){
+    File dataBaseName = new File();
+    if(dataBaseName.exists(){
+      System.out.println("Please stoperino before you make a duplicate filerino");
+    }else dataBaseName.createNewFile();
   }
 
   public static void dropDatabase(String dataBaseName){
-    
+    if(dataBaseName.exists(){
+        dataBaseName.delete();
+        System.out.println(dataBaseName + " has been deleted");
+    }else System.out.println("That doesn't exist my guy");    
   }
 
   public static void saveDatabase(String dataBaseName){
